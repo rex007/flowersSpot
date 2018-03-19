786617b80f1036c71d4ee43fb99bb36ee5147c10
Andraz <info@kuret.me>
2018-03-19 19:21:19 +0100

    add test for index

--- spec/api/v1/sightings/comments_controller_spec.rb
+++ spec/api/v1/sightings/comments_controller_spec.rb
@@ -3,8 +3,23 @@ require 'rails_helper'
 RSpec.describe Api::V1::Sightings::CommentsController, type: :controller do
 
   describe 'index' do
-    
+    it 'should return comments for sightings' do
+      user = create(:user)
+      flower = create(:flower)
+      sighting = create(:sighting, user: user, flower: flower)
+      comment1 = create(:comment, user: user, sighting: sighting)
+      comment2 = create(:comment, user: user, sighting: sighting)
+
+      request.headers['Authorization'] = user.to_jwt
+      get :index, params: {sighting_id: sighting.id}
+      expect(response.status).to eq(200)
+      results = JSON.load(response.body)
+      expect(results['comments'].count).to eq(2)
+    end
   end
+
+
+
 
   describe 'create' do
   	it 'comment for sighting' do
@@ -13,6 +28,7 @@ RSpec.describe Api::V1::Sightings::CommentsController, type: :controller do
       
       comment_params = {
         user_id: user.id,
+        sighting_id: sighting.id,
         body: 'Lorem ipsom'
       }
 
