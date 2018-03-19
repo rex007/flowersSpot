01d2d8606de54ff0d45329febfa9e20fcfa34e52
Andraz <info@kuret.me>
2018-03-19 10:17:14 +0100

    fix flower controller for passing the tests

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,7 +4,10 @@ class Api::V1::ApplicationController < ActionController::API
     # current page, previous page, next page, total pages
     {
       pagination: {
-        # ...
+        current_page: paginated_obj.current_page,
+        previous_page: paginated_obj.prev_page,
+        next_page: paginated_obj.next_page,
+        total_pages: paginated_obj.total_pages 
       }
     }
   end
--- app/controllers/api/v1/flowers_controller.rb
+++ app/controllers/api/v1/flowers_controller.rb
@@ -10,10 +10,10 @@ class Api::V1::FlowersController < Api::V1::BaseController
   end
 
   def search
-    @flowers = Flower.alphabetical.page(params[:page]).per(params[:per_page])
+    flowers = Flower.arel_table
+    @flowers = Flower.page.where(flowers[:name].matches("%#{params[:query]}%"))
     render json: @flowers,
       root: :flowers,
-      meta: generate_pagination(@flowers),
-      each_serializer: FlowersSerializer
+      meta: generate_pagination(@flowers)
   end
 end
