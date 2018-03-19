e5ffd1654bc2aa1a1285993246bc58c7f9c70299
Andraz <info@kuret.me>
2018-03-19 10:46:01 +0100

    add custom rescue handlers

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  rescue_from ActionController::ParameterMissing, :with => :parameter_missing
 
   def generate_pagination(paginated_obj)
     # current page, previous page, next page, total pages
@@ -11,4 +13,15 @@ class Api::V1::ApplicationController < ActionController::API
       }
     }
   end
+
+  private
+
+    def record_not_found(error)
+      render :json => {:error => error.message}, :status => 404
+    end 
+
+    def parameter_missing(error)
+      render :json => {:error => error.message}, :status => 400
+    end
+  
 end
