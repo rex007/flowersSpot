81886e006fdfac1e5ce763fee6fd0c88060fd81d
Andraz <info@kuret.me>
2018-03-19 10:12:26 +0100

    first init

---
+++ README.md
@@ -0,0 +1,13 @@
+# FlowrSpot API (test build) v.2
+
+<img src="./erd.png">
+
+## Info
+
+1. This project was build with Ruby 2.3.4 & Rails 5.1.4.
+1. Copy .env.sample to .env file and modify if necessary.
+1. Modify the database.yml file if needed.
+1. Create the database together with the migrations.
+1. Run `rake db:test:prepare` and to run `rspec spec` to run the tests (they will fail, read further down the assigment).
+1. We are using JWT for Authentication.
+1. For tests we have RSpec and a modified file to run Rubocop to check the code.
---
+++ app/controllers/api/v1/sightings_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::SightingsController < Api::V1::BaseController
+  skip_before_action :authenticate_api_user, only: [:index, :show]
+
+  def index
+    @sightings = Sighting.latest.page(params[:page]).per(params[:per_page])
+    render json: @sightings,
+      root: :sightings,
+      meta: generate_pagination(@sightings),
+      each_serializer: SightingsSerializer
+  end
+
+  def show
+    @sighting = Sighting.find params[:id]
+    render json: @sighting, serializer: SightingSerializer
+  end
+
+  def create
+    @sighting = Sighting.new(sighting_params)
+    @sighting.user_id = @current_user.id
+    if @sighting.save
+      render json: @sighting, serializer: SightingSerializer
+    else
+      render json: { error: @sighting.errors.full_messages },
+             status: 400
+    end
+  end
+
+  def update
+    @sighting = @current_user.sightings.find params[:id]
+    if @sighting.update(sighting_params)
+      render json: @sighting, serializer: SightingSerializer
+    else
+      render json: { error: @sighting.errors.full_messages },
+             status: 400
+    end
+  end
+
+  def destroy
+    @sighting = @current_user.sightings.find params[:id]
+    @sighting.destroy
+    render json: @sighting, serializer: SightingSerializer
+  end
+
+  protected
+
+  def sighting_params
+    params.permit(:flower_id, :user_id, :name, :description, :latitude,
+      :longitude, :picture)
+  end
+end
