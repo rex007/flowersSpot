292e549cddd0b7ce2b0c64a60d71660dde39b995
Andraz <info@kuret.me>
2018-03-20 08:54:58 +0100

    add index create destroy for likes

--- app/controllers/api/v1/sightings/likes_controller.rb
+++ app/controllers/api/v1/sightings/likes_controller.rb
@@ -1,3 +1,35 @@
 class Api::V1::Sightings::LikesController < Api::V1::Sightings::BaseController
-  
+	before_action :set_sighting, only: [:index, :create, :destroy]
+
+	def index
+		@likes = @sighting.likes.order(created_at: :desc)
+		render json: @likes
+	end
+
+	def create
+		@like = @sighting.likes.new(like_params)
+		@like.user_id = @current_user.id
+		if @like.save
+			render json: @like
+		else
+			render json: {error: @like.errors.full_messages}, status: 400
+		end
+	end
+
+	def destroy
+	    @like = @current_user.likes.find(params[:id])
+	    @like.destroy
+	    render json: @like		
+	end
+
+
+	private
+
+	def set_sighting
+		@sighting = Sighting.find(params[:sighting_id])
+	end
+
+	def like_params
+		params.require(:like).permit(:user_id, :sighting_id)
+	end
 end
